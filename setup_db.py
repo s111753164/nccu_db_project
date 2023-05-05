f2280cc57e3816fa0b7b7337314553cf269000ea
s111753164 <111753164@g.nccu.edu.tw>
2023-05-05 15:57:03 +0800

    0505

--- setup_db.py
+++ setup_db.py
@@ -33,7 +33,7 @@ with db:
     db.executescript(create_db_sql)
     cursor = db.cursor()
     cursor.execute(
-        'INSERT INTO staffs(sname, empid, password) VALUES ("dylan","s111753164", "1234")'
+        'INSERT INTO staffs(sname, empid, password) VALUES ("Lisa","s111753164", "1234")'
     )
 
 db = sqlite3.connect('reports.db')
